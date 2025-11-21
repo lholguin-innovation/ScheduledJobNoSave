2b1d071b327882ca95b62c94c06b62f1f0cdb17b
lholguin-innovation <LHOLGUIN@LEAR.COM>
2025-11-21 15:09:48 -0700

    Added delay config

--- ScheduledTask/Form1.Designer.cs
+++ ScheduledTask/Form1.Designer.cs
@@ -32,12 +32,25 @@
             button1 = new Button();
             timer1 = new System.Windows.Forms.Timer(components);
             textBox1 = new TextBox();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            button2 = new Button();
+            statusStrip1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
             button1.BackColor = Color.Lime;
-            button1.Location = new Point(330, 331);
+            button1.Location = new Point(326, 295);
             button1.Name = "button1";
             button1.Size = new Size(135, 64);
             button1.TabIndex = 0;
@@ -52,21 +65,111 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 12);
+            textBox1.Location = new Point(3, 2);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(776, 303);
+            textBox1.Size = new Size(781, 287);
             textBox1.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 393);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(39, 17);
+            toolStripStatusLabel1.Text = "Status";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 393);
+            tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 365);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Execution";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(numericUpDown1);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 365);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Config";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Delay (min)";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(38, 43);
+            numericUpDown1.Maximum = new decimal(new int[] { 60, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 0;
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(224, 43);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Save";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 407);
-            Controls.Add(textBox1);
-            Controls.Add(button1);
+            ClientSize = new Size(800, 415);
+            Controls.Add(tabControl1);
+            Controls.Add(statusStrip1);
             Name = "Form1";
             Text = "Guardar No. Trabajo";
+            Load += Form1_Load;
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -76,5 +179,13 @@
         private Button button1;
         private System.Windows.Forms.Timer timer1;
         private TextBox textBox1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private Button button2;
     }
 }
--- ScheduledTask/Form1.cs
+++ ScheduledTask/Form1.cs
@@ -1,45 +1,51 @@
 using Microsoft.Data.SqlClient;
+using System.Configuration;
 
 namespace ScheduledTask
 {
     public partial class Form1 : Form
     {
-      //  string conStr = "Data Source=mxchlac-sql04;Initial Catalog=PBS;User ID=sqlIT;Password=";
+        //  string conStr = "Data Source=mxchlac-sql04;Initial Catalog=PBS;User ID=sqlIT;Password=";
         string conStr = "Server=mxchlac-sql04;Database=LAIMS;User Id=sqlIIT;Password=Le@r2025!iitdata;MultipleActiveResultSets=True;TrustServerCertificate=True;";
-        int fMin = 5*60  ; // 5 MIN
+        int fMin = 5 * 60; // 5 MIN
         int curr = 0;
         bool running = false;
         public Form1()
         {
             InitializeComponent();
             timer1.Start();
+            fMin = Int32.Parse(numericUpDown1.Value.ToString()) * 60;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            running  = !running;
+            running = !running;
             if (running)
             {
                 curr = fMin;
                 button1.BackColor = Color.Red;
-                button1.Text = "End"; 
+                button1.Text = "End";
+                toolStripStatusLabel1.Text = "Running";
             }
-            else {
+            else
+            {
                 button1.BackColor = Color.Lime;
                 button1.Text = "Start";
+                toolStripStatusLabel1.Text = "Stoped";
             }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if(curr >= fMin)
+            if (running)
             {
-                curr = 0;
-                if(running  )
+                toolStripStatusLabel1.Text = fMin - curr + " seconds until next execution";
+                if (curr >= fMin)
                 {
+                    curr = 0;
                     callProcedure();
                 }
-                }
+            }
             curr++;
         }
         private void callProcedure()
@@ -59,13 +65,38 @@ namespace ScheduledTask
             }
             catch (Exception ex)
             {
-                textBox1.Invoke(() => { textBox1.AppendText("[" + D.ToShortTimeString() + "] "+ "Error:" + ex.Message + Environment.NewLine); });
+                textBox1.Invoke(() => { textBox1.AppendText("[" + D.ToShortTimeString() + "] " + "Error:" + ex.Message + Environment.NewLine); });
             }
             finally
             {
-                if(connection.State == System.Data.ConnectionState.Open) 
+                if (connection.State == System.Data.ConnectionState.Open)
                     connection.Close();
             }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            fMin = Int32.Parse(numericUpDown1.Value.ToString()) * 60;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fMin = Properties.Settings.Default.Delay;
+
+            try
+            {
+                numericUpDown1.Value = fMin;
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Delay = fMin;
+           Properties.Settings.Default.Save();
         }
     }
 }
